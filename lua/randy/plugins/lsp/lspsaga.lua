e580539ef9bfdce9b8c9df2a4c436325b6133548
randyydoo <randyydoo@gmail.com>
2024-02-17 15:02:25 -0800

    removed lightbulb

--- lua/randy/plugins/lsp/lspsaga.lua
+++ lua/randy/plugins/lsp/lspsaga.lua
@@ -1,10 +1,11 @@
 -- import lspsaga safely
 local saga_status, saga = pcall(require, "lspsaga")
 if not saga_status then
-  return
+	return
 end
 
 saga.setup({
-  -- keybinds for navigation in lspsaga window
-  move_in_saga = { prev = "<C-k>", next = "<C-j>" },
+	ui = { code_action = "" },
+	-- keybinds for navigation in lspsaga window
+	move_in_saga = { prev = "<C-k>", next = "<C-j>" },
 })
