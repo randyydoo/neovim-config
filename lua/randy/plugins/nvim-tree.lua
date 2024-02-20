684029370714abe466e85a6f5c33000dce17341c
randyydoo <randyydoo@gmail.com>
2024-02-19 23:17:51 -0800

    added index markers to tree

--- lua/randy/plugins/nvim-tree.lua
+++ lua/randy/plugins/nvim-tree.lua
@@ -1,6 +1,6 @@
 local setup, nvimtree = pcall(require, "nvim-tree")
 if not setup then
-  return
+	return
 end
 
 -- recommended settings from nvim-tree documentation
@@ -8,4 +8,31 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 -- change color for arrows in tree to light blue
-nvimtree.setup()
+nvimtree.setup({
+	renderer = {
+		add_trailing = false,
+		group_empty = false,
+		full_name = false,
+		root_folder_label = ":~:s?$?/..?",
+		indent_width = 2,
+		special_files = { "Cargo.toml", "Makefile", "README.md", "readme.md" },
+		symlink_destination = true,
+		highlight_git = "none",
+		highlight_diagnostics = "none",
+		highlight_opened_files = "none",
+		highlight_modified = "none",
+		highlight_bookmarks = "none",
+		highlight_clipboard = "name",
+		indent_markers = {
+			enable = true,
+			inline_arrows = true,
+			icons = {
+				corner = "└",
+				edge = "│",
+				item = "│",
+				bottom = "─",
+				none = " ",
+			},
+		},
+	},
+})
