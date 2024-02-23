f517de9ebfc1321151f9e9d3b9758cf607e689c6
randyydoo <randyydoo@gmail.com>
2024-02-23 12:39:14 -0800

    removed scope and edited indent line color

--- lua/randy/plugins/indent-lines.lua
+++ lua/randy/plugins/indent-lines.lua
@@ -1,1 +1,18 @@
-require("ibl").setup({})
+local highlight = {
+	"grey",
+}
+
+local hooks = require("ibl.hooks")
+-- create the highlight groups in the highlight setup hook, so they are reset
+-- every time the colorscheme changes
+hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+	vim.api.nvim_set_hl(0, "grey", { fg = "#403d3d" })
+end)
+
+require("ibl").setup({
+	indent = { highlight = highlight },
+	scope = {
+		enabled = false,
+		show_start = false,
+	},
+})
